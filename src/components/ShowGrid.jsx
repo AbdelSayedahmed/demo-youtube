94c4cf5e66c931ff82df34dab3ededbd19feef0e
AbdelSayedahmed <abdelrahman.s2003@icloud.com>
2024-05-12 17:41:04 -0400

    css revamp

--- src/components/ShowGrid.jsx
+++ src/components/ShowGrid.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import ShowListing from "./ShowListing";
+import "./ShowGrid.css"
 
 export default function ShowGrid({ items }) {
   return (
     <div className="shows-container">
-      {items.map((item) => (
-        <Link key={item.videoId} to={`/${item.videoId}`}>
+      {items.map((item, i) => (
+        <Link key={i} to={`/${item.videoId}`}>
           <ShowListing title={item.title} thumbnail={item.thumbnail} />
         </Link>
       ))}
