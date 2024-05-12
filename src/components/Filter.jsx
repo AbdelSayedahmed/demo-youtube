29d8db6fe2bbd5db68f1f1051e4ff4013010929d
AbdelSayedahmed <abdelrahman.s2003@icloud.com>
2024-05-12 14:57:26 -0400

    submit search working

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -3,11 +3,17 @@ import "./Filter.css";
 
 export default function Filter({ handleFilter }) {
   return (
-    <input
-      id="filter-input"
-      type="text"
-      placeholder="Search"
-      onChange={(e) => handleFilter(e.target.value)}
-    />
+    <form className="filter-form">
+      <div className="search-container">
+        <input
+          type="text"
+          placeholder="Search.."
+          onSubmit={(e) => handleFilter(e.target.value)}
+        />
+        <button type="submit">
+          <i className="fa fa-search"></i>
+        </button>
+      </div>
+    </form>
   );
 }
